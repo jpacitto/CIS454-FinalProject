14af999600adf481e74ab9f5be34f02f75cdfef7
jpacitto <jpacitto@umassd.edu>
2018-12-08 21:06:29 -0500

    Initial Push

---
+++ game.js
@@ -0,0 +1,46 @@
+var cameraRotate = 0.005;
+var score = document.getElementById("score");
+
+var animate = function()
+{
+	requestAnimationFrame(animate);
+	cube.rotation.x += 0.0005;
+
+
+	updateScore();
+
+
+    camera.lookAt(scene.position);
+	renderer.render(scene, camera);
+}
+
+animate();
+
+var oldTime = 0;
+function updateScore()
+{
+	var date = new Date();
+	var time = date.getTime();
+
+	if(time - oldTime > 100)
+	{
+		num = score.textContent;
+		num = parseInt(num) + 1;
+		score.innerHTML = num;
+		oldTime = time;
+	}
+}
+
+document.addEventListener('keydown', function(event){
+	if(event.keyCode == 87 && xwing.position.y < 9.5){
+		xwing.position.y += 0.5;
+	}else if(event.keyCode == 65 && xwing.position.x > -11){
+		xwing.position.x -= 0.5;
+	}else if(event.keyCode == 83 && xwing.position.y > -8){
+		xwing.position.y -= 0.5;
+	}else if(event.keyCode == 68 && xwing.position.x < 10.5){
+		xwing.position.x += 0.5;
+	}
+
+	console.log(xwing.position.x.toString() + ", " + xwing.position.y.toString());
+})
---
+++ project2.js
@@ -0,0 +1,63 @@
+var xwing;
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+camera.lookAt(scene.position);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0x818080);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var canvas = document.getElementById("canvas");
+canvas.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+var controls = new THREE.OrbitControls(camera, renderer.domElemet);
+controls.enableDamping = true;
+controls.campingFactor = 0.25;
+controls.enableZoom = true;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100,0,100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
+
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+ambientLight.position.y = 100;
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var skyBoxGeo = new THREE.CubeGeometry(1000, 1000, 1000);
+var cubeMaterials = 
+[
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/memes.jpg"), side: THREE.DoubleSide}),
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/cwd_bk.jpg"), side: THREE.DoubleSide}),
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/cwd_dn.jpg"), side: THREE.DoubleSide}),
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/cwd_lf.jpg"), side: THREE.DoubleSide}),
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/cwd_rt.jpg"), side: THREE.DoubleSide}),
+	new THREE.MeshBasicMaterial( { map : new THREE.TextureLoader().load("res/sor_cwd/cwd_up.jpg"), side: THREE.DoubleSide})
+];
+
+var cube = new THREE.Mesh(skyBoxGeo, cubeMaterials);
+scene.add(cube);
+
+var objLoader = new THREE.ObjectLoader();
+objLoader.load('res/x-wing/star-wars-x-wing.json', function(object)
+{
+	xwing = object;
+	scene.add(xwing);
+	xwing.position.z = -10;
+	xwing.position.y = -5;
+	xwing.rotation.y = 3.15;
+});
