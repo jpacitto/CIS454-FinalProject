1ff87e11f01a5376f874b2869441984a1183748d
jpacitto <jpacitto@umassd.edu>
2018-12-11 18:00:32 -0500

    Added collision and models

--- game.js
+++ game.js
@@ -1,18 +1,34 @@
+//Listen for key inputs
 document.addEventListener('keydown', function(event){
 	if(event.keyCode == 87 && xwing.position.y < 9.5){
-		xwing.position.y += 0.5;
-	}else if(event.keyCode == 65 && xwing.position.x > -11){
-		xwing.position.x -= 0.5;
+		xwing.position.y += 0.7;
+		cube1.position.y += 0.7;
+	}else if(event.keyCode == 65 && xwing.position.x > -17){
+		xwing.position.x -= 0.7;
+		cube1.position.x -= 0.7;
 	}else if(event.keyCode == 83 && xwing.position.y > -8){
-		xwing.position.y -= 0.5;
-	}else if(event.keyCode == 68 && xwing.position.x < 10.5){
-		xwing.position.x += 0.5;
+		xwing.position.y -= 0.7;
+		cube1.position.y -= 0.7;
+	}else if(event.keyCode == 68 && xwing.position.x < 17){
+		xwing.position.x += 0.7;
+		cube1.position.x += 0.7;
+	}else if(event.keyCode == 72){
+		if(mat.transparent == true){
+			mat.transparent = false;
+			mat.opacity = 1;
+		}else{
+			mat.transparent = true;
+			mat.opacity = 0;
+		}
 	}
+
 });
 
+//variables for game difficulty
 var cameraRotate = 0.005;
 var score = document.getElementById("score");
 var spawnRate = 5000;
+var objSpeed = 0.5;
 
 var fileLoc = [
 	'res/tiefighter/starwars-tie-fighter.json',
@@ -23,44 +39,101 @@ var oldTime = 0;
 var lastObject = 0;
 
 var obstacles = new Array();
+var spin = new Array();
+var collidableMeshList = [];
 
+//game loop
+var id;
 var animate = function()
 {
 	var date = new Date();
 	var time = date.getTime();
 
-	requestAnimationFrame(animate);
+	id = requestAnimationFrame(animate);
 
+	//loading objects at specific rate
 	if(time - lastObject > spawnRate)
 	{
-		var num = Math.floor(Math.random() * 2);
+		var num = Math.floor(Math.random() * 4);
 		lastObject = time;
 
-		objLoader.load(fileLoc[num], function(object){
+		var spinning = -1;
+		var obj;
+		var hitbox;
+		if(num == 0){
+			obj = tiefigher.clone();
+			hitbox = tieCube.clone();
+		}else if(num == 1){
+			obj = shrek.clone();
+			hitbox = shrekCube.clone();
+			spinning = 1;
+		}else if(num == 2){
+			obj = vader.clone();
+			hitbox = vaderCube.clone();
+		}else if(num == 3){
+			spinning = 1;
+			obj = asteroid.clone();
+			hitbox = astroCube.clone();
+		}
 
-			if(num == 0){
-				scaleFactor = 0.5;
-			}else if(num == 1){
-				scaleFactor = 0.05;
-			}
+		var y = Math.floor(Math.random() * Math.floor(21)) - 10;
+		var x = Math.floor(Math.random() * Math.floor(28)) - 17;
 
-			var y = Math.floor(Math.random() * 10)
+		obj.position.x = x;
+		obj.position.y = y;
+		obj.position.z = -100;
 
-			object.position.z = -50;
-			object.scale.set(scaleFactor, scaleFactor, scaleFactor);
-			scene.add(object);
-			obstacles.push(object);
-		});
+		hitbox.position.x = x;
+		hitbox.position.y = y;
+		hitbox.position.z = -100;
+		
+		var p = -1;
+		if(spinning == 1)
+		{
+			p = Math.floor(Math.random() * Math.floor(3));
+		}
+
+		scene.add(obj);
+		scene.add(hitbox)
+		collidableMeshList.push(hitbox);
+		obstacles.push(obj);
+		spin.push(p);
+
+
 	}
 
+	//remove object from list
 	if(lastObject != 0)
 	{
 		for(var i = 0; i < obstacles.length; i++)
 		{
-			obstacles[i].position.z += 0.5;
+			obstacles[i].position.z += objSpeed;
+			collidableMeshList[i].position.z += objSpeed;
+
+			if(spin[i] == 0)
+			{
+				obstacles[i].rotation.z += 0.01;
+				obstacles[i].rotation.x += 0.01;
+				collidableMeshList[i].rotation.z += 0.01;
+				collidableMeshList[i].rotation.x += 0.01;
+			}else if(spin[i] == 1){
+				obstacles[i].rotation.z += 0.01;
+				obstacles[i].rotation.y += 0.01;
+				collidableMeshList[i].rotation.z += 0.01;
+				collidableMeshList[i].rotation.y += 0.01;
+			}else if(spin[i] == 2){
+				obstacles[i].rotation.x += 0.01;
+				obstacles[i].rotation.y += 0.01;
+				collidableMeshList[i].rotation.x += 0.01;
+				collidableMeshList[i].rotation.y += 0.01;
+			}
+
 			if(obstacles[i].position.z > 30){
 				scene.remove(obstacles[i]);
+				scene.remove(collidableMeshList[i]);
 				obstacles.splice(i, 1);
+				spin.splice(i, 1);
+				collidableMeshList.splice(i, 1);
 			}
 		}
 	}
@@ -71,18 +144,44 @@ var animate = function()
 	///////////////////////////////////
 	//////////////Dont' touch/////////
 	cube.rotation.x += 0.0005;
-	if(time - oldTime > 100)
+	if(time - oldTime > 50)
 	{
 		num = score.textContent;
-		if(parseInt(num) % 200 == 0 && num != 0){
-			spawnRate -= 500;
+		if(parseInt(num) % 200 == 0 && num != 0 && spawnRate > 1000){
+			spawnRate -= 1000;
+			objSpeed += 0.05;
+		}
+
+		if(parseInt(num) == 1000)
+		{
+			spawnRate = 600;
 		}
 		num = parseInt(num) + 1;
 		score.innerHTML = num;
 		oldTime = time;
 	}
 
-    camera.lookAt(scene.position);
+
+	////////
+	////////Collision Detection
+	var finalScore = score.textContent;
+	var originPoint = cube1.position.clone()
+	for (var vertexIndex = 0; vertexIndex < cube1.geometry.vertices.length; vertexIndex++)
+	{       
+		var localVertex = cube1.geometry.vertices[vertexIndex].clone();
+		var globalVertex = localVertex.applyMatrix4(cube1.matrix);
+		var directionVector = globalVertex.sub( cube1.position );
+
+		var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+		var collisionResults = ray.intersectObjects( collidableMeshList );
+		if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
+		{
+			cancelAnimationFrame(id);
+			score.innerHTML = "GAME OVER! Score: " + finalScore;
+		}
+	}
+
+	camera.lookAt(scene.position);
 	renderer.render(scene, camera);
 }
 
--- project2.js
+++ project2.js
@@ -1,27 +1,87 @@
-var xwing, tiefigher, asteroid, shrek;
+var xwing, tiefigher, vader, shrek, asteroid;
 
+var objLoader = new THREE.ObjectLoader();
+
+//shrek loader
+objLoader.load('res/shrek/backdoor.json', function(object)
+{
+	shrek = object;
+	shrek.scale.set(0.05, 0.05, 0.05);
+});
+
+//tiefighter loader
+objLoader.load('res/tiefighter/starwars-tie-fighter.json', function(object)
+{
+	tiefigher = object;
+	tiefigher.scale.set(0.5, 0.5, 0.5);
+});
+
+objLoader.load('res/vaderfighter/star-wars-vader-tie-fighter.json', function(object){
+	vader = object;
+	vader.scale.set(0.3,0.3,0.3);
+});
+
+objLoader.load('res/asteroid/asteroid.json', function(object){
+	asteroid = object;
+	asteroid.scale.set(0.3 ,0.3, 0.3);
+});
+
+//create the scene
 var scene = new THREE.Scene();
+
+//var mat = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var mat = new THREE.MeshBasicMaterial( {color: 0x00ff00, transparent: true, opacity: 0} );
+
+var shrekGeo = new THREE.BoxGeometry( 7, 6, 0.5);
+var shrekCube = new THREE.Mesh( shrekGeo, mat );
+
+var tieGeo = new THREE.BoxGeometry( 3, 4, 4);
+var tieCube = new THREE.Mesh( tieGeo, mat );
+
+var vaderGeo = new THREE.BoxGeometry(7,6,10);
+var vaderCube = new THREE.Mesh(vaderGeo, mat);
+
+var astroGeo = new THREE.BoxGeometry(10,6,7);
+var astroCube = new THREE.Mesh(astroGeo, mat);
+
+var geometry = new THREE.BoxGeometry( 7, 1, 9);
+var cube1 = new THREE.Mesh( geometry, mat );
+scene.add( cube1 );
+
+objLoader.load('res/x-wing/star-wars-x-wing.json', function(object)
+{
+	xwing = object;
+	xwing.rotation.y = 3.15;
+	xwing.scale.set(0.75, 0.75, 0.75);
+	scene.add(xwing);
+});
+
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.z = 20;
 camera.position.y = 6;
 camera.lookAt(scene.position);
 
+//display scene on screen
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0x818080);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 var canvas = document.getElementById("canvas");
 canvas.appendChild(renderer.domElement);
 
+
+//create skybox
 var geometry = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
+//add orbit controls
 var controls = new THREE.OrbitControls(camera, renderer.domElemet);
 controls.enableDamping = true;
 controls.campingFactor = 0.25;
 controls.enableZoom = true;
 
+
+//lighting
 var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 keyLight.position.set(-100, 0, 100);
 
@@ -39,6 +99,7 @@ scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
 
+//load skybox textures
 var skyBoxGeo = new THREE.CubeGeometry(1000, 1000, 1000);
 var cubeMaterials = 
 [
@@ -52,12 +113,3 @@ var cubeMaterials =
 
 var cube = new THREE.Mesh(skyBoxGeo, cubeMaterials);
 scene.add(cube);
-
-var objLoader = new THREE.ObjectLoader();
-objLoader.load('res/x-wing/star-wars-x-wing.json', function(object)
-{
-	xwing = object;
-	xwing.rotation.y = 3.15;
-	scene.add(xwing);
-	console.log(xwing);
-});
